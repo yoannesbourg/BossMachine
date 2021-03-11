f5e478bf2497fde48146f5f9fe253c85f65f5b59
Yoann <yoannpascalbouix@gmail.com>
2021-03-11 15:08:17 +0100

    Delete minion

--- server/minions.js
+++ server/minions.js
@@ -23,3 +23,33 @@ minionsRouter.param('minionId', (req, res, next, id) => {
 minionsRouter.get('/', (req, res, next) => {
     res.send(getAllFromDatabase('minions'))
 })
+
+minionsRouter.post('/', (req, res, next) => {
+    const newMinion = addToDatabase('minions', req.body)
+    res.status(201).send(newMinion)
+})
+
+minionsRouter.get('/:minionId', (req, res, next) => {
+  res.send(req.minion);
+});
+
+minionsRouter.put('/:minionId', (req, res, next) => {
+    let updatedMinionInstance = updateInstanceInDatabase('minions', req.body)
+    res.send(updatedMinionInstance)
+});
+
+minionsRouter.put('/:minionId', (req, res, next) => {
+    let updatedMinionInstance = updateInstanceInDatabase('minions', req.body)
+    res.send(updatedMinionInstance)
+});
+
+minionsRouter.delete('/:minionId', (req, res, next) => {
+    const deleted = deleteFromDatabasebyId('minions', req.params.minionId);
+    if (deleted) {
+      res.status(204);
+    } else {
+      res.status(500);
+    }
+    res.send();
+  });
+ 
