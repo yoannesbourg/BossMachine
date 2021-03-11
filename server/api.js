c09c3c397dbd03dcf7746d8b05df3a08dd1e0cb5
Yoann <yoannpascalbouix@gmail.com>
2021-03-11 15:32:02 +0100

    Meetings api

--- server/api.js
+++ server/api.js
@@ -2,8 +2,10 @@ const express = require('express');
 const apiRouter = express.Router();
 
 const minionsRouter = require('./minions')
+const meetingsRouter = require('./meetings')
 
 apiRouter.use('/minions', minionsRouter)
+apiRouter.use('/meetings', meetingsRouter)
 
 
 module.exports = apiRouter;
